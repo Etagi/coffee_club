61da650e2ac649ed88efd43f8cd668cfa0105ebe
Dmitriy Krivenko <d.n.krivenko@gmail.com>
2016-04-29 09:13:53 +0500

    Payment form

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,2 +1,25 @@
 class PaymentsController < ApplicationController
+  def new
+    @payment = Payment.new
+  end
+
+  def create
+    @payment = Payment.create(payment_params)
+  end
+
+  def index
+    @payments = Payment.all
+  end
+
+  def show
+    @payment = Payment.find(params[:id])
+  end
+
+  def destroy
+    @payment = Payment.find(params[:id])
+  end
+
+  def payment_params
+    params.require(:payment).permit(:from_user_id, :money)
+  end
 end
